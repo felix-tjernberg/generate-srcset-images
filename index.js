61cffa0308b353889eb7ad59add6f60d6089ef19
FilleFLEX <felix.tjernberg@hotmail.se>
2021-08-13 18:08:56 +0200

    Add test-image file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const txtOutput = {}
 
 // Add your original image path and desired sizes/format
-import testImage from './image/test-image.png?sizes[]=300,sizes[]=600,sizes[]=1024,sizes[]=2048&format=webp'
+import testImage from './image/test-image.jpg?sizes[]=300,sizes[]=600,sizes[]=1024,sizes[]=2048&format=webp'
 
 // Add your image srcset string to txtOutput object
 txtOutput['test-image'] = testImage.srcSet
